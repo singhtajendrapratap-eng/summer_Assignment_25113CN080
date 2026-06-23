2f31613fd86272f87a85239102963ff1d3fed8b5
TAJENDRA PRATAP SINGH <singhtajendrapratap@gmail.com>
2026-06-23 19:26:58 +0530

    Day-22 assignment is uploaded

--- Day-22/87.py
+++ Day-22/87.py
@@ -1,1 +1,7 @@
 #write a program to character frequency.
+s=input("Enter a string:")
+freq={}
+for ch in s:
+    freq[ch]=freq.get(ch,0)+1
+for key, value in freq.items():
+    print(key,":",value)
--- Day-22/88.py
+++ Day-22/88.py
@@ -1,1 +1,4 @@
 #write a program to remove spaces from string.
+s=input("Enter a string:")
+result=s.replace(" ","")
+print("After removing spaces:",result)
